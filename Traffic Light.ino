ecc6e5c09af7633d8b2bc067130a1802ce43a930
TarikEskiyurt <74590497+TarikEskiyurt@users.noreply.github.com>
2021-06-12 23:22:22 +0300

    Add files via upload

---
+++ Traffic Light.ino
@@ -0,0 +1,22 @@
+void setup() {            //At this part, we're describing pins.
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(8, HIGH); //At here, we give energy to pin.
+  delay(3000);           //At here, it is waiting for 3000 ms.
+  digitalWrite(8, LOW);  //At here, we cut energy from pin.
+  digitalWrite(9, HIGH);
+  delay(1000);
+  digitalWrite(9, LOW);
+  digitalWrite(10, HIGH);
+  delay(3000);
+  digitalWrite(10, LOW);
+  digitalWrite(9, HIGH);
+  delay(1000);
+  digitalWrite(9, LOW);
+
+}
