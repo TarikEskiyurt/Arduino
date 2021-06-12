c80cb59f03d939993a34aa9841dd48ea9abde85d
TarikEskiyurt <74590497+TarikEskiyurt@users.noreply.github.com>
2021-06-12 21:53:03 +0300

    Arduino Projects
    I publish at here my arduino project's codes. I hope you'll like it.

---
+++ blink.ino
@@ -0,0 +1,12 @@
+void setup() {
+  pinMode(8, OUTPUT);   //At this part, we're describing pin.
+
+}
+
+void loop() {
+  digitalWrite(8, HIGH);    //At here, we give energy to pin.
+  delay(100);               //At here, it is waiting for 100 ms.
+  digitalWrite(8, LOW);     //At here, we cut energy from pin.
+  delay(100);
+
+}
---
+++ buton.ino
@@ -0,0 +1,13 @@
+void setup() {
+  pinMode(8, OUTPUT);
+  pinMode(9, INPUT);
+
+}
+
+void loop() {
+ if (digitalRead(9)==1)
+ digitalWrite(8, HIGH);
+ else
+ digitalWrite(8, LOW);
+
+}
