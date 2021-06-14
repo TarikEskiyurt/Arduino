440c1603566ebd8e728fcba07999b02b1f630a10
TarikEskiyurt <74590497+TarikEskiyurt@users.noreply.github.com>
2021-06-14 21:33:46 +0300

    Add files via upload

---
+++ led_with_potansiometer.ino
@@ -0,0 +1,15 @@
+void setup() {
+  Serial.begin(9600); 
+  Serial.println("Potansiometer value reading : ");
+  
+
+}
+
+void loop() {
+  int deger = analogRead(A0);  //We say "set deger variable to a0 pin's value"
+  deger = deger/4;
+  analogWrite(9, deger);
+  Serial.println(deger);  //We say "print value to serial monitor"
+  delay(500);   //We say "wait 500 ms"
+
+}
